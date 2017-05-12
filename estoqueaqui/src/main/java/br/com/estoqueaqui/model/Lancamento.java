cf9cef114c93f177523d0adf1f80caa929baf3c4
felipedossantos896 <felipedossantos896@gmail.com>
2017-05-11 23:01:23 -0300

    Adicionando a biblioteca apache.poi, implementação do botão para exportação de PDF.

--- estoqueaqui/src/main/java/br/com/estoqueaqui/model/Lancamento.java
+++ estoqueaqui/src/main/java/br/com/estoqueaqui/model/Lancamento.java
@@ -3,13 +3,27 @@ package br.com.estoqueaqui.model;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Lancamento implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
+	@ManyToOne
 	private Material material;
+	
+	@Column(name="data_lancamento")
 	private Date dataLancamento;
+	
 	private Integer quantidade;
 
 	public Long getId() {
